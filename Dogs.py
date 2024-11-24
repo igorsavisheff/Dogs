09d9ffd64a3e3f3232ff30d2998ec3b14a4beeec
IGOR <arseniy.savisheff@gmail.com>
2024-11-24 14:10:53 +0300

    добавлен прогресс-бар

--- Dogs.py
+++ Dogs.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter import messagebox as mb
 import requests
 from PIL import Image, ImageTk
@@ -7,10 +8,10 @@ from io import BytesIO
 
 def get_dog_image():
     try:
-        response = requests.get("https://dog.ceo/api/breeds/random")
+        response = requests.get("https://dog.ceo/api/breeds/image/random")
         response.raise_for_status()
         data = response.json()
-        return data('message')
+        return data['message']
     except Exception as e:
         mb.showerror("Ошибка", f"Возникла ошибка при запросе к API {e}")
         return None
@@ -27,21 +28,30 @@ def show_image():
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
             img.thumbnail((300, 300))
-            img = ImageTk,PhotoImage(img)
+            img = ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image = img
         except Exception as e:
             mb.showerror("Ошибка", f"Возникла ошибка при загрузке изображения{e}")
+    progress.stop()
+
+def prog():
+    progress['value']=0
+    progress.start(30)
+    window.after(3000,show_image)
 
 
 window = Tk()
 window.title("Картинки с собачками")
 window.geometry("360x420")
 
-label = Label()
+label = ttk.Label()
 label.pack(pady=10)
 
-button = Button(text="Загрузить изображение", command=show_image)
+button = ttk.Button(text="Загрузить изображение", command=prog)
 button.pack(pady=10)
+
+progress = ttk.Progressbar(mode="determinate",length=300)
+progress.pack(pady=10)
 
 window.mainloop()
